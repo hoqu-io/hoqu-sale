f523ca38943f1c4b35dc59da489a3971069e34b1
Aleksandr Krivtcun <a.krivtcun@gmail.com>
2017-11-22 11:30:33 +0300

    add changeable rate crowdsale contract

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,9 +1,29 @@
 var HoQuToken = artifacts.require("../HoQuToken.sol");
+var ChangeableRateCrowdSale = artifacts.require("../ChangeableRateCrowdsale.sol");
 
 module.exports = function(deployer, network, accounts) {
   if (network === 'development') {
     const totalSupply = web3.toWei("888888000", "Ether");
+    const bankAddress = web3.eth.accounts[1];
+    const beneficiaryAddress = web3.eth.accounts[2];
+    const tokenRate = 6000; // 1 ETH = 6000 HQX
+    const minBuyableAmount = web3.toWei("1", "Ether");
+    const maxTokensAmount = web3.toWei("34666632", "Ether");
+    const endDate = 1513296000;
 
-    deployer.deploy(HoQuToken, totalSupply);
+    deployer.deploy(HoQuToken, totalSupply).then(function () {
+      return deployer.deploy(HoQuToken, totalSupply)
+    }).then(function () {
+      return deployer.deploy(
+        ChangeableRateCrowdSale,
+        HoQuToken.address,
+        bankAddress,
+        beneficiaryAddress,
+        tokenRate,
+        minBuyableAmount,
+        maxTokensAmount,
+        endDate
+      )
+    });
   }
 };
